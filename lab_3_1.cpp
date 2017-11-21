170d14ac65d00b241373568aa1218b58559b72ff
Unknown <titirat339@gmail.com>
2017-11-21 17:29:55 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Miss." << "Titirat Sopharat" << endl;
+  cout << "Nickname : " << "Mo." << endl;
+  cout << "Sex : " << "female" << endl;
+  cout << "Date of Birth : " << "August 23, 1998"  << endl;
+  cout << "Hobbies : " << "play a game"  << endl;
+  cout << "Status : " << "Single"  << endl;
   return 0;
 }
